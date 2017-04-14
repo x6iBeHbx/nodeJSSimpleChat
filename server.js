30d1ca5e7e0388702e48041f3920261597e74bb2
Andrii Andriushchenko <aandriushchenko@playtika.com>
2017-04-14 15:05:40 +0300

    finished simple chat

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.send(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('message: '+msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
-	console.log('listen on * 3000');
+  console.log('listening on *:3000');
 });
+
